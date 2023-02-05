ba05b87772fd22d54bfdf424c69a0b7f20af4eb5
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-02-05 05:47:24 +0100

    Fix command

--- demo/__main__.py
+++ demo/__main__.py
@@ -106,13 +106,10 @@ class Composer:
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url")
-parser.add_argument("project_name")
 
 args = parser.parse_args()
 
 
-PROJECT_PATH = DATA_PATH / args.project_name
-PROJECT_PATH.mkdir(exist_ok=True)
+(d := Downloader(args.url)).download_all()
 
-
-Downloader(args.url).download_all()
+Composer(d.project_id).compose()
