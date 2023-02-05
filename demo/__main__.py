869b724b872ddec043f761d82b8ead52dba32a0c
Elia Giacobazzi <elia.giacobazzi@gmail.com>
2023-02-05 05:47:12 +0100

    Compose deskshare and webcam

--- demo/__main__.py
+++ demo/__main__.py
@@ -3,11 +3,10 @@ import pathlib
 import re
 import urllib.parse
 import urllib.request
+import xml.etree.ElementTree as ET
 
+import moviepy.editor
 import requests
-
-# import xml.etree.ElementTree as ET
-
 
 DATA_PATH = pathlib.Path(".data")
 
@@ -58,14 +57,51 @@ class Downloader:
         for endpoint in (
             # "panzooms.xml",
             # "cursor.xml",
-            # "deskshare.xml",
+            "deskshare.xml",
             # "presentation_text.json",
             # "captions.json",
             # "slides_new.xml",
-            # "video/webcams.webm",
-            # "deskshare/deskshare.webm",
+            "video/webcams.webm",
+            "deskshare/deskshare.webm",
         ):
             self.download(endpoint)
+
+
+class Composer:
+    def __init__(self, project_id: str):
+        self._id = project_id
+        self._base_dir = DATA_PATH / self._id
+
+        self.size = (1920, 1080)
+        self._clips = []
+
+        self._prepare_webcam()
+        self._prepare_deskshare()
+
+    def compose(self):
+        if self._clips:
+            movie = moviepy.editor.CompositeVideoClip(self._clips, size=self.size)
+            movie = movie.set_duration(30)
+            movie.write_videofile(str(self._base_dir / "out.mp4"))
+
+    def _prepare_webcam(self):
+        video_url = str(self._base_dir / "video" / "webcams.webm")
+        video = moviepy.editor.VideoFileClip(video_url)
+        self._clips.append(video)
+
+    def _prepare_deskshare(self):
+        events = ET.parse(self._base_dir / "deskshare.xml").findall("./event")
+        if not events:
+            return
+
+        video_url = str(self._base_dir / "deskshare" / "deskshare.webm")
+        video = moviepy.editor.VideoFileClip(video_url)
+
+        for event in events:
+            t_start = float(event.get("start_timestamp"))
+            t_stop = float(event.get("stop_timestamp"))
+            clip = video.subclip(t_start, t_stop)
+            self._clips.append(clip.set_position((self.size[0] - clip.size[0], 0)))
 
 
 parser = argparse.ArgumentParser()
